3908eced9f5ef99d21ad7a75b886579a7fb35d74
Alexey Shevchenko <kamikozz@mail.ru>
2021-04-20 02:52:48 +0300

    feat: add relations between business processes on Graph component

--- src/components/Graph/Graph.tsx
+++ src/components/Graph/Graph.tsx
@@ -2,7 +2,7 @@ import React, { useState, useRef, useEffect } from 'react';
 
 import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
 
-import { useAppDispatch, useAppSelector } from '../../app/hooks';
+import { useAppSelector } from '../../app/hooks';
 import { selectRoles } from '../../features/roles/rolesSlice';
 import { selectActions } from '../../features/actions/actionsSlice';
 import { selectRoleActionMap } from '../../features/roleActionMap/roleActionMapSlice';
@@ -28,14 +28,13 @@ const useStyles = makeStyles((theme: Theme) =>
       justifyContent: 'center',
       minWidth: 30,
       padding: '25px 0px',
-      // overflow: 'hidden',
       writingMode: 'vertical-lr',
       transform: 'rotate(180deg)',
-      border: '1px solid black',
+      borderLeft: '1px solid black',
     },
     processesContainer: {
       display: 'flex',
-      // flexWrap: 'wrap',
+      alignItems: 'center',
     },
     process: {
       display: 'flex',
@@ -66,137 +65,96 @@ export default function RelationsCreator() {
   const actions = useAppSelector(selectActions);
   const roleActionMap = useAppSelector(selectRoleActionMap);
   const bpRelations = useAppSelector(selectBPRelations);
-  const dispatch = useAppDispatch();
 
-  const fromRef = useRef<HTMLDivElement | null>(null);
-  const toRef = useRef<HTMLDivElement | null>(null);
+  const itemsRef = useRef<any>({});
 
-  const getPointCenterCoords = (ref: any) => {
-    if (!ref || !ref.current) return [undefined, undefined];
-    const element = ref.current;
+  const rolesArrayIds = Object.keys(roles);
+  const roleActionMapArrayIds = Object.keys(roleActionMap);
+  const bpRelationsArrayIds = Object.keys(bpRelations);
+
+  const getPointCenterCoords = (element: any) => {
     const { offsetLeft, offsetWidth, offsetTop, offsetHeight } = element;
     const x = Number(offsetLeft) + Number(offsetWidth) / 2;
     const y = Number(offsetTop) + Number(offsetHeight) / 2;
     return [x, y];
   };
 
-  const [centerX, centerY] = getPointCenterCoords(fromRef);
-  const [centerToX, centerToY] = getPointCenterCoords(toRef);
-
-  let width;
-  let height;
-
-  if (centerX !== undefined
-    && centerToX !== undefined
-    && centerY !== undefined
-    && centerToY !== undefined) {
-    width = Math.max(centerX, centerToX);
-    height = Math.max(centerY, centerToY);
-  }
-
-  const rolesArrayIds = Object.keys(roles);
-  const roleActionMapArrayIds = Object.keys(roleActionMap);
-  // console.log(bpRelations, roleActionMapArrayIds, rolesArrayIds);
-  // console.log(roleActionMap[roleActionMapArrayIds[0]], roles);
-  // console.log(roleActionMapArrayIds, roleActionMap[roleActionMapArrayIds[0]]);
-
-  const isRefsInitialized = width !== undefined && height !== undefined;
-
   return (
-    <>
-      <div className={classes.root}>
-        {
-          rolesArrayIds.map((roleId) => {
-            const { name: roleName } = roles[roleId];
-            return (
-              <div key={roleId} className={classes.workspace}>
-                <div className={classes.role}>{roleName}</div>
-                <div className={classes.processesContainer}>
-                  {
-                    roleActionMapArrayIds.map((roleActionRelationId) => {
-                      const roleActionRelation = roleActionMap[roleActionRelationId];
-                      const { actionId, roleId: relationRoleId } = roleActionRelation;
-                      if (roleId === relationRoleId) {
-                        const { name: actionName } = actions[actionId];
-                        return (
-                          <div key={roleActionRelationId} ref={fromRef} className={classes.process}>{actionName}</div>
-                        );
-                      }
-                    })
-                  }
-                </div>
+    <div className={classes.root}>
+      {
+        rolesArrayIds.map((roleId) => {
+          const { name: roleName } = roles[roleId];
+          return (
+            <div key={roleId} className={classes.workspace}>
+              <div className={classes.role}>{roleName}</div>
+              <div className={classes.processesContainer}>
+                {
+                  roleActionMapArrayIds.map((roleActionRelationId) => {
+                    const roleActionRelation = roleActionMap[roleActionRelationId];
+                    const { actionId, roleId: relationRoleId } = roleActionRelation;
+                    if (roleId === relationRoleId) {
+                      const { name: actionName } = actions[actionId];
+                      return (
+                        <div
+                          key={roleActionRelationId}
+                          ref={(el) => itemsRef.current[roleActionRelationId] = el}
+                          className={classes.process}
+                        >{actionName}</div>
+                      );
+                    }
+                  })
+                }
               </div>
+            </div>
+          );
+        })
+      }
+      {
+        bpRelationsArrayIds
+          .map((bpRelationId) => {
+            const [relationFromId, relationToId] = bpRelations[bpRelationId].relation;
+
+            const relationFromEl = itemsRef.current[relationFromId];
+            const relationToEl = itemsRef.current[relationToId];
+
+            const [centerX, centerY] = getPointCenterCoords(relationFromEl);
+            const [centerToX, centerToY] = getPointCenterCoords(relationToEl);
+
+            const markerWidth = 7;
+            const markerHeight = 7;
+
+            const width = Math.max(centerX, centerToX) + markerWidth;
+            const height = Math.max(centerY, centerToY) + markerHeight;
+
+            const pointsPolyline = [
+              `${centerX},${centerY}`,
+              `${(centerX + centerToX) / 2},${(centerY + centerToY) / 2}`,
+              `${centerToX},${centerToY}`
+            ].join(' ');
+
+            return (
+              <svg
+                key={bpRelationId}
+                className={classes.svg}
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox={`0 0 ${width} ${height}`}
+                width={width}
+                height={height}
+              >
+                <defs>
+                  <marker id="arrowhead" viewBox="0 0 10 10" refX="3" refY="5"
+                  markerWidth={markerWidth} markerHeight={markerHeight} orient="auto">
+                    <path fill="white" stroke="black" d="M 0 0 L 10 5 L 0 10 z" />
+                  </marker>
+                </defs>
+                <g fill="none" stroke="black" strokeWidth="2" markerMid="url(#arrowhead)">
+                  <polyline points={pointsPolyline} // d={`M ${centerX},${centerY} L ${centerToX},${centerToY}`}
+                  />
+                </g>
+              </svg>
             );
           })
-        }
-        {
-          isRefsInitialized &&
-          <svg
-            className={classes.svg}
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox={`0 0 ${width} ${height}`}
-            width={width}
-            height={height}
-          >
-            <defs>
-              <marker id="arrowhead" viewBox="0 0 10 10" refX="3" refY="5"
-              markerWidth="7" markerHeight="7" orient="auto">
-                <path fill="white" stroke="black" d="M 0 0 L 10 5 L 0 10 z" />
-              </marker>
-            </defs>
-            <g fill="none" stroke="black" strokeWidth="2" markerMid="url(#arrowhead)">
-              <polyline
-                points={`${centerX},${centerY} ${(Number(centerX) + Number(centerToX)) / 2},${(Number(centerY) + Number(centerToY)) / 2} ${centerToX},${centerToY}`}
-                // d={`M ${centerX},${centerY} L ${centerToX},${centerToY}`}
-              />
-            </g>
-          </svg>
-        }
-      </div>
-
-{/*
-      <div className={classes.root}>
-        <div className={classes.workspace}>
-          <div className={classes.role}>Кто-то</div>
-          <div ref={fromRef} className={classes.process}>
-            Сделать что-то
-          </div>
-        </div>
-
-        <div className={classes.workspace}>
-          <div className={classes.role}>Гриша</div>
-          <div className={classes.process}>
-            Сделать что-то2
-          </div>
-          <div ref={toRef} className={classes.process}>
-            Сделать что-то3
-          </div>
-        </div>
-
-        {
-          isRefsInitialized &&
-          <svg
-            className={classes.svg}
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox={`0 0 ${width} ${height}`}
-            width={width}
-            height={height}
-          >
-            <defs>
-              <marker id="arrowhead" viewBox="0 0 10 10" refX="3" refY="5"
-              markerWidth="7" markerHeight="7" orient="auto">
-                <path fill="white" stroke="black" d="M 0 0 L 10 5 L 0 10 z" />
-              </marker>
-            </defs>
-            <g fill="none" stroke="black" strokeWidth="2" markerMid="url(#arrowhead)">
-              <polyline
-                points={`${centerX},${centerY} ${(Number(centerX) + Number(centerToX)) / 2},${(Number(centerY) + Number(centerToY)) / 2} ${centerToX},${centerToY}`}
-                // d={`M ${centerX},${centerY} L ${centerToX},${centerToY}`}
-              />
-            </g>
-          </svg>
-        }
-      </div> */}
-    </>
+      }
+    </div>
   );
 }
